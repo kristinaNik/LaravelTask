a9667bff90cdf0b5af2f065522b3ce21e94e1018
kristinaNik <kristina.nik93@gmail.com>
2021-02-18 17:38:38 +0200

    Added some changes to the tests

--- src/laravel/tests/Unit/RabbitmqConnectionTest.php
+++ src/laravel/tests/Unit/RabbitmqConnectionTest.php
@@ -6,7 +6,6 @@ namespace Tests\Unit;
 use App\Services\RabbitMQServices\Consumer;
 use App\Services\RabbitMQServices\HandleConnection;
 use App\Services\RabbitMQServices\Producer;
-use Mockery;
 use Tests\TestCase;
 
 class RabbitmqConnectionTest extends TestCase
@@ -16,9 +15,20 @@ class RabbitmqConnectionTest extends TestCase
     {
         $connection = new HandleConnection();
 
-        $this->assertEquals(60,  $connection::connect()->getHeartbeat());
-        $this->assertEquals(60, $connection::connect()->getInsist());
-        $this->assertEquals(60, $connection->connect()->getLoginMethod());
+        $this->assertEquals(30,  $connection::connect()->getHeartbeat());
+        $this->assertEquals(30, $connection::connect()->getInsist());
+        $this->assertEquals(15, $connection->connect()->getLoginMethod());
         $this->assertEquals('/', $connection->connect()->getVhost());
+    }
+
+    public function testMessage()
+    {
+        $message = json_encode(['message' => 'hi']);
+        $connection = new HandleConnection();
+
+        $producer = new Producer($connection);
+        $consumer = new Consumer($connection);
+        $producer->produce($message);
+        $consumer->consume();
     }
 }
